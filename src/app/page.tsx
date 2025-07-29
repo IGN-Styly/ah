657a92a8e8490ebf3ce5c0d7da170309d78efb9c
IGN-Styly <claudioresendeptbr@gmail.com>
2025-07-29 22:40:17 +0100

    hero start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar";
 import { example, NBTDisplay, parseNBT } from "@/components/nbt";
 import SignIn from "@/components/signIn";
 import { ModeToggle } from "@/components/theme";
+import { buttonVariants } from "@/components/ui/button";
 import {
   HoverCard,
   HoverCardContent,
@@ -15,9 +16,12 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { authClient } from "@/lib/auth-client";
+import AppConfig from "@/lib/config";
+import { cn } from "@/lib/utils";
 import { api } from "@convex/_generated/api";
 import { useQuery } from "convex/react";
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default function Home() {
@@ -29,10 +33,37 @@ export default function Home() {
     <div className=" ">
       <Navbar></Navbar>
       <main className="container mx-auto px-6 py-8">
-        <div className="mb-8">
-          <h1 className="text-3xl font-bold text-foreground mb-2">
-            AuctionHouse
-          </h1>
+        <div className="mb-8 flex flex-box justify-center">
+          <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+            <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+              <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold">
+                Auction House
+              </h1>
+              <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+                AuctionHouse is a reactive realtime application that allows the
+                exchange of NBT Items in an open market for coins.
+              </p>
+              <div className="space-x-4">
+                <Link
+                  href="/signin"
+                  className={cn(buttonVariants({ size: "lg" }), "rounded-none")}
+                >
+                  Get Started
+                </Link>
+                <Link
+                  href={AppConfig.GITHUB}
+                  target="_blank"
+                  rel="noreferrer"
+                  className={cn(
+                    buttonVariants({ variant: "outline", size: "lg" }),
+                    "rounded-none",
+                  )}
+                >
+                  GitHub
+                </Link>
+              </div>
+            </div>
+          </section>
         </div>
       </main>
     </div>
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,7 @@
 const TAX = 0.025;
+const GITHUB = "https://github.com/IGN-Styly/ah";
 const AppConfig = {
   TAX,
+  GITHUB,
 };
 export default AppConfig;
